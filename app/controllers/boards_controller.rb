f728fa8eda1caf0a019836c0932e771f4ca098a7
ry0559 <r08014019606@gmail.com>
2020-02-15 01:46:48 +0900

    initial commit devise機能

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,9 @@
+class BoardsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+  def index  
+  end
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+end
